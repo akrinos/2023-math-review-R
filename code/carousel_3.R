c28bfdca6f8cfef01f0df5d2249278c29a7ee464
Arianna Krinos <akrinos@vt.edu>
2023-08-15 23:09:43 -0400

    adding plotting output

--- code/carousel_3.R
+++ code/carousel_3.R
@@ -5,10 +5,26 @@
 #' I have some data where I have two True/False values: 
 #' whether it rained on a given day and whether temperature 
 #' exceeded 80F. 
-#' I want to plot a histogram of each distribution of values 
+#' I want to plot a bar plot of each distribution of values 
 #' alongside a heatmap that shows the number of times these 
 #' two events occurred simultaneously. How can I do this 
 #' efficiently?
 #' 
 #' 
 #' The data for this exercise is stored in `../data/carousel_2.csv`
+
+carousel_3 = read.csv("data/carousel_3.csv")
+
+head(carousel_3)
+
+# `p_load` from `dplyr` allows to to simultaneously install.packages + library
+pacman::p_load(dplyr)
+
+carousel_3 |> group_by(Rained,TempAbove80) |> dplyr::tally()
+
+pacman::p_load(ggplot2)
+
+pdf("output/rain_bar.pdf")
+barplot(formula=n~Rained,
+        data=(carousel_3 |> group_by(Rained) |> dplyr::tally()))
+dev.off()
