99fc496861b0b2020a2e3fcf1bb1a2003877a4b4
Arianna Krinos <akrinos@vt.edu>
2023-08-15 23:24:24 -0400

    finishing README updates

--- code/carousel_3.R
+++ code/carousel_3.R
@@ -17,8 +17,16 @@ carousel_3 = read.csv("data/carousel_3.csv")
 
 head(carousel_3)
 
+## how could we end up with grouped data the hard way?
+categories=unique(carousel_3$Rained)
+new_result_frame=data.frame()
+for (category in categories) {
+  new_result_frame=rbind(new_result_frame,data.frame("Rained"=category,
+                                                     "Count"=nrow(carousel_3[carousel_3$Rained==category,])))
+}
+
 # `p_load` from `dplyr` allows to to simultaneously install.packages + library
-pacman::p_load(dplyr)
+pacman::p_load(dplyr,tidyverse)
 
 carousel_3 |> group_by(Rained,TempAbove80) |> dplyr::tally()
 
@@ -28,3 +36,10 @@ pdf("output/rain_bar.pdf")
 barplot(formula=n~Rained,
         data=(carousel_3 |> group_by(Rained) |> dplyr::tally()))
 dev.off()
+
+for_heatmap = carousel_3 |> group_by(Rained,TempAbove80) |> dplyr::tally() |>
+  tidyr::pivot_wider(names_from="Rained",id_cols="TempAbove80",values_from="n")
+heatmap(as.matrix(for_heatmap |> tibble::column_to_rownames("TempAbove80")))
+
+ggplot(carousel_3 |> group_by(Rained,TempAbove80) |> dplyr::tally()) + 
+    geom_tile(aes(x=Rained,y=TempAbove80,fill=n))
